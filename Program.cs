3458966b105421dfc25acd605c46870f91378a90
unknown <aniamilton129@gmail.com>
2024-03-21 00:58:00 +0200

    updated main

--- Program.cs
+++ Program.cs
@@ -4,8 +4,17 @@ namespace Exam_Unit_Three
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Functions.Square(5));
-            Console.WriteLine(Functions.InchesToMm(10));
+            //Console.WriteLine(Popping_Functions.Square(5));
+            //Console.WriteLine(Popping_Functions.InchesToMm(10));
+            //Console.WriteLine(My_Books.BooksAfter1992());
+            //Console.WriteLine(My_Books.BooksBefore2004());
+            //Console.WriteLine(string.Join("\n", My_Books.IsbnByAuthor("Brandon Sanderson")));
+            //Console.WriteLine(string.Join("\n", My_Books.Alphabetical(true).Select(b => b.title)));
+            //Console.WriteLine(Left_Right_Up_Down.Nodes(Left_Right_Up_Down.LoadNodes()));
+            //Console.WriteLine(Left_Right_Up_Down.Deepest(Left_Right_Up_Down.LoadNodes()));
+            //Console.WriteLine(Left_Right_Up_Down.Sum(Left_Right_Up_Down.LoadNodes()));
+            int[] flattened = Flattened_Numbers.FetchAndFlattenArray().GetAwaiter().GetResult();
+            Console.WriteLine(string.Join(", ", flattened));
         }
     }
 }
