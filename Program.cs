323470ba1deee10fee6e41f28dbbbf2a43ec9bac
unknown <aniamilton129@gmail.com>
2024-03-20 17:48:07 +0200

    Made main function

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace Exam_Unit_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Functions.Square(5));
+            Console.WriteLine(Functions.InchesToMm(10));
+        }
+    }
+}
