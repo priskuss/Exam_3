725f1ea6272281de34aafde4893774eb31a5a57c
unknown <aniamilton129@gmail.com>
2024-03-21 14:36:28 +0200

    added tests

--- Program.cs
+++ Program.cs
@@ -4,17 +4,26 @@ namespace Exam_Unit_Three
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(Popping_Functions.Square(5));
-            //Console.WriteLine(Popping_Functions.InchesToMm(10));
-            //Console.WriteLine(My_Books.BooksAfter1992());
-            //Console.WriteLine(My_Books.BooksBefore2004());
-            //Console.WriteLine(string.Join("\n", My_Books.IsbnByAuthor("Brandon Sanderson")));
-            //Console.WriteLine(string.Join("\n", My_Books.Alphabetical(true).Select(b => b.title)));
-            //Console.WriteLine(Left_Right_Up_Down.Nodes(Left_Right_Up_Down.LoadNodes()));
-            //Console.WriteLine(Left_Right_Up_Down.Deepest(Left_Right_Up_Down.LoadNodes()));
-            //Console.WriteLine(Left_Right_Up_Down.Sum(Left_Right_Up_Down.LoadNodes()));
-            int[] flattened = Flattened_Numbers.FetchAndFlattenArray().GetAwaiter().GetResult();
-            Console.WriteLine(string.Join(", ", flattened));
+            Test.SquareTest();
+            Test.InchesToMmTest();
+            Test.RootTest();
+            Test.CubeTest();
+            Test.CircleAreaTest();
+            Test.GreetingTest();
+            Test.FlattenedNumbersTest();
+            Test.TheBooksTest();
+            Test.TAuthorsTest();
+            Test.BooksAfter1992Test();
+            Test.BooksBefore2004Test();
+            Test.IsbnByAuthorTest();
+            Test.AlphabeticalOrderTest();
+            Test.ChronologicalOrderTest();
+            Test.GroupByFirstNameTest();
+            Test.GroupByLastNameTest();
+            Test.LoadNodesTest();
+            Test.SumTest();
+            Test.DeepestTest();
+            Test.NodesTest();
         }
     }
 }
